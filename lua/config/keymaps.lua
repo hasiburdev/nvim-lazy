566f723bb28b2cdb63c46b68d2fd28146cded56a
Hasibur Rahman <hasiburdev@gmail.com>
2023-09-14 00:27:43 +0600

    Init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+
+-- Insert mode
+vim.keymap.set("i", "jj", "<Esc>")
+
+-- Normal mode
+vim.keymap.set("n", "<leader>s", ":w<cr>")
+
+-- Visual mode
